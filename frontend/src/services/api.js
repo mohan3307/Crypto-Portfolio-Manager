daf006709d5f4a0d857d88261ca3115807802f8a
mohan3307 <mohanrajm03032007@gmail.com>
2026-03-11 21:27:18 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({ baseURL: 'https://crypto-portfolio-manager-1.onrender.com/api' });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
